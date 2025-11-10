e7cafa528e288c9fa44f0aa37cd668255493aaa7
shayanscience <shayancomeon@gmail.com>
2025-11-10 11:23:21 -0800

    Update: Print craft name, time of execution - Add execute_function()-> This looks for an update in mission_control and     executes the update. - Add a never ending loop continously run certain commands and check if there     exists a new update for execution.

--- main.py
+++ main.py
@@ -7,7 +7,18 @@
 
 # Libraries
 import time
+import sys
+sys.path.append("C:/Users/ShayanSheikhrezaei/OneDrive - Orca Technologies/Personal/Projects/Programming/Py_mission_control_project01")
+import missionControl
+import os
 
+
+#Messages Flag
+update_flag = True
+file_execution_flag = False
+
+#directories
+mission_control = r"C:\Users\ShayanSheikhrezaei\OneDrive - Orca Technologies\Personal\Projects\Programming\Py_mission_control_project01\missionControl.py"
 
 # testing
 print("Hello World!")
@@ -15,8 +26,35 @@ print("Hello World!")
 
 # Firmware Greeting - executes once
 print("**** Craft Oblivious ****")
-execution_date = time.localtime()
-formatted = time.strftime("%Y-%m-%d", execution_date)
+execution_date = time.localtime()                       # Get the local time
+formatted = time.strftime("%Y-%m-%d", execution_date)   # Convert time to formatted time (interested in date)
 print(f"**** Date: {formatted} ****")
 
-# print(time.asctime()[0])
+
+#executables
+def execute_update():
+    global update_flag, file_execution_flag
+    if (os.path.exists(mission_control)):
+        update_flag = True
+        if(file_execution_flag==False):
+            file_execution_flag = True
+            print("There is an update available!")
+            time.sleep(1)
+            print("Fetching the file...")
+            time.sleep(3)
+            missionControl.update01()
+    else:
+        if(update_flag):
+            print("No update available!")
+            print("Continouing previous instructions")
+            update_flag = False
+
+# test_formatted = time.strftime("%H:%M:%S", execution_date)
+while True:
+    execute_update()
+
+    execution_date = time.localtime() 
+    test_formatted = time.strftime("%H:%M:%S", execution_date)  
+    print("Time: ", test_formatted)
+    time.sleep(5)
+
